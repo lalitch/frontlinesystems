a5598e79011974b4b0da0b3b35c43d463a76eb26
Lalit Chourey <lalitchourey@Lalits-MacBook-Pro.local>
2018-11-19 20:46:08 -0800

    Changes

---
+++ src/main/java/com/plessentials/frontlinesystems/controllers/TestController.java
@@ -0,0 +1,22 @@
+package com.plessentials.frontlinesystems.controllers;
+
+import com.plessentials.frontlinesystems.models.User;
+import com.plessentials.frontlinesystems.utils.RestClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public User test() {
+        RestClient<User> restClient = new RestClient<User>(User.class);
+        return restClient.get(URI.create("https://putsreq.com/kow1qydD9w4HiQjkNTzl"));
+    }
+
+    @GetMapping("/welcome")
+    public String test1() {
+        return "Welcome";
+    }
+}
--- src/main/java/com/plessentials/frontlinesystems/controllers/UsersController.java
+++ src/main/java/com/plessentials/frontlinesystems/controllers/UsersController.java
@@ -3,16 +3,9 @@ package com.plessentials.frontlinesystems.controllers;
 import com.plessentials.frontlinesystems.dataproviders.DynamoDBDataProvider;
 import com.plessentials.frontlinesystems.dataproviders.IDataProvider;
 import com.plessentials.frontlinesystems.models.User;
-import com.plessentials.frontlinesystems.utils.RestClient;
 import com.plessentials.frontlinesystems.utils.Validations;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -20,7 +13,7 @@ public class UsersController {
     private IDataProvider<User> userDataProvider;
 
     public UsersController() {
-        this.userDataProvider = new DynamoDBDataProvider<>(User.class);
+        // this.userDataProvider = new DynamoDBDataProvider<>(User.class);
     }
 
     @PostMapping()
@@ -42,12 +35,6 @@ public class UsersController {
         User user = new User(id);
 
         this.userDataProvider.delete(user);
-    }
-
-    @GetMapping("/test")
-    public User test() {
-        RestClient<User> restClient = new RestClient<User>(User.class);
-        return restClient.get(URI.create("https://putsreq.com/kow1qydD9w4HiQjkNTzl"));
     }
 }
 
